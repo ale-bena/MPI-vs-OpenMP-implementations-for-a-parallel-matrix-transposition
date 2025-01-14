a8412eb5a0e4247a8a036306e41e58cd73df217b
ale-bena <127418373+ale-bena@users.noreply.github.com>
2025-01-14 10:15:45 +0100

    Update mpifunc.h

--- source/mpifunc.h
+++ source/mpifunc.h
@@ -1,1 +1,49 @@
+#ifndef MPIFUNC_H_
+#define MPIFUNC_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <string.h>
+
+int isPowerOfTwo(int n); //verifies if the number is a power of 2
+
+float random_float(int min, int max); //generates random float numbers
+
+float** declareMatrix(int n); //declares the matrix
+
+void initializeMatrix(float** M, int n); //initializes the matrix with random numbers
+
+int checkSymOMP(float** M, int n); //checks if the matrix is symmetric
+
+void matTransposeOMP(float** M, float** T, int n, int blockSize);  //transposes the matrix using blocked approach
+
+void printMatrix(float **M, int n); //prints the matrix, to see if the program works
+
+void freeMemory(float** M, int n); //frees the allocated memory
+
+void bubbleSort(double sort[], int n);  //bubble sort function for a double array
+
+double timeAverage(double sort[], int n); //computes the average of the 40% of the elemnts of the array
+
+void checkCorrectness(float **M, float** T, int n);  //checks if the transposition has been executed correctly
+
+double speedup(double timeSeq, double timeAvg);
+
+double efficiency(double speedup, int nthreads);
+
+void cache_flush();
+
+
+void mpiMatrixTranspose(int n, int rank, int size);
+
+void matTranspose(float** M, float** T, int n);
+
+void localTranspose(float* local_columns, float* local_transposed, int cols_per_process, int n);
+
+int checkSym(float** M, int n);
+
+void freeMatrix(float** M);
+
+
+#endif
