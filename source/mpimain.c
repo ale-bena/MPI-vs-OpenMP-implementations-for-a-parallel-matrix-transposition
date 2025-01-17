b307a3dc25ba185dda2e41a0022a5ddc90a13080
ale-bena <127418373+ale-bena@users.noreply.github.com>
2025-01-17 22:34:34 +0100

    Update mpimain.c

--- source/mpimain.c
+++ source/mpimain.c
@@ -2,101 +2,132 @@
 #define MAX 128
 
 int main(int argc, char** argv){
+//get arguments: dim e thread(gia presi con MPI)
+	if(argc<3){
+			printf("Insert a number!!");
+			exit(1);
+	}
+  int n = atoi(argv[1]);
+  if(isPowerOfTwo(n)==0){
+      exit(1);
+  }
+  int tries=atoi(argv[2]);
+  if(tries<1){
+      exit(1);
+  }
+
+//init MPI environment
     int rank, size;
-    int n = 16; // Matrix size (n x n)
     srand(25);
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // Ensure the number of processes divides n evenly
-    if (n % size != 0 && rank == 0) {
-        fprintf(stderr, "Matrix size %d must be divisible by the number of processes %d.\n", n, size);
-        MPI_Finalize();
-        return EXIT_FAILURE;
+// Ensure the number of processes divides n evenly
+    if(size>n || n%size!=0){
+      if(rank==0){
+        printf("can't run because of size%d>n%d or n not multiple", size, n);
+      }
+      MPI_Finalize();
+      return 0;
     }
-
-    // Perform matrix transposition with MPI
-    mpiMatrixTranspose(n, rank, size);
-
-    MPI_Finalize();
-
-/*	if(argc<2){
-		printf("Insert a number!!");
-		exit(1);
-	}
-	int n=atoi(argv[1]);
-	if(isPowerOfTwo(n)==0){
-		exit(1);
-	}
-	
-	int n_threads=atoi(argv[2]);
-	if(n_threads<1){
-		exit(1);
-	}
-	omp_set_num_threads(n_threads);
-	
-	int tries=atoi(argv[3]);
-	if(tries<1){
-		exit(1);
-	}
-	
-	srand(25);
-	double s,e, time_elapsed;
-	const char *times_table="times_table.csv";
-	const char *header1 = "Type, Dim, Threads, Time";
-	FILE *times = fopen(times_table, "a");
-	if (times == NULL) {
-		perror("Error opening file");
-    exit(1); 
-	}
-
-//SORT ARRAY INITIALIZATION
-	double *sort = (double *)malloc(tries*sizeof(double));
-	if(sort==NULL){
-		printf("Memory allocation failed!!\n");
-		free(sort);
-		exit(1);
-	}
- 
-   switch(n){
-     case 16: blocksize=8; break;
-     case 32: blocksize=16; break;
-     case 64: blocksize=32; break;
-     default: blocksize=64;
-   }
- 
-//EXECUTION
-	for (int i=0; i<tries; i++){
-	  cache_flush();
-		float** M=declareMatrix(n);
-		initializeMatrix(M, n);
-		float** T=declareMatrix(n);
-		s=omp_get_wtime();
-		matTransposeOMP(M,T,n,blocksize);
-		e=omp_get_wtime();
+//variables
+    double s,e, time_elapsed, time_avg;
+/*		const char *times_table="times_table.csv";
+		FILE *times = fopen(times_table, "a");
+		if (times == NULL) {
+			printf("Error opening file");
+		exit(1); 
+    }
+    */
+		double *sort; // = (double *)malloc(tries*sizeof(double));
+    if(rank==0){
+      sort = (double *)malloc(tries*sizeof(double));
+		  if(sort==NULL){
+			  printf("Memory allocation failed!!\n");
+			  free(sort);
+			  exit(1);
+	    }
+    }
+ 	float** M = NULL;//declareMatrix(n);
+	float** T = NULL;//declareMatrix(n);
+// 
+for(int i=0; i<tries; i++){
+    cache_flush();
+	//float** M = NULL;//declareMatrix(n);
+	//float** T = NULL;//declareMatrix(n);
+   M=declareMatrix(n);
+   T=declareMatrix(n);
+   //initializeMatrix(M, n); 
+	if (rank == 0) {
+       // M = declareMatrix(n); 
+       initializeMatrix(M, n); 
+       //printMatrix(M,n);
+		   // T = declareMatrix(n);
+        s = MPI_Wtime();
+    }
+  //if(mpicheckSym(M, n ,rank, size)==0){
+    mpiMatrixTranspose(M, T, n, rank, size);
+ // }
+ // else{printf("\nERRORRE\n\n");}
+   
+	if(rank==0){
+		e = MPI_Wtime();
+     //printMatrix(T, n);
+    /*if(n<=32){
+      checkCorrectness(M,T,n);
+    }*/
+		const char *times_table="times_table.csv";
+		FILE *times = fopen(times_table, "a");
+		if (times == NULL) {
+			printf("Error opening file");
+		exit(1); 
+    }
+    
 		time_elapsed = (e-s);
 		sort[i]=time_elapsed;
-		fprintf(times, "BlockOmp,%d,%d,%.9f\n",n, n_threads, time_elapsed);		
-		freeMemory(M, n);
-		freeMemory(T,n);
-	} 
-  fclose(times);
- 	bubbleSort(sort, tries);
- 	double time_avg = timeAverage(sort, tries); 
+		fprintf(times, "MPI,%d,%d,%.9f\n",n, size, time_elapsed);
+    //printf("\nTime elapsed: %.9f\n", time_elapsed);		
+	  //freeMatrix(M);
+	  //freeMatrix(T);
+     fclose(times);
+    }
+    freeMatrix(M);
+    freeMatrix(T);
+}
+// --------------------------
+//sort and average
+	//fclose(times);
+/*	if(rank==0){
+ 	  bubbleSort(sort, tries);
+    time_avg = timeAverage(sort, tries);
+  }
+
   const char *times_average="times_average.csv";
-	const char *header2 = "Type, Dim, Threads, Time, Speedup, Efficiency";
 	FILE *avg = fopen(times_average, "a+");
 	if (avg == NULL) {
-		perror("Error opening file");
+		printf("Error opening file");
 		return 1;
-	}
+  }*/
 
+if(rank==0){
+    bubbleSort(sort, tries);
+    //for(int i=0; i<tries; i++){printf("%.9f ", sort[i]);}
+    time_avg = timeAverage(sort, tries);
+    //printf("time average: %.9f\n", time_avg);
+  	
+     const char *times_average="times_average.csv";
+	FILE *avg = fopen(times_average, "a+");
+	if (avg == NULL) {
+		printf("Error opening file");
+		return 1;
+  }
+  
 	char buffer[MAX];
 	char string[] = "Sequential";
 	double gettime = 0.0;
-	fgets(buffer, MAX, times);
-	while(fgets(buffer, MAX, times)){
+	fgets(buffer, MAX, avg);
+	while(fgets(buffer, MAX, avg)){
 		char *token = strtok(buffer, ",");
 		int i=0;
 		int flag=0;
@@ -118,28 +149,36 @@ int main(int argc, char** argv){
 					flag=0;
 				} 
 			}
-			
 			if(i==4 && flag==1){
+      //printf("..");
 				gettime=atof(token);
 			}
 			token = strtok(NULL, ",");
 		}
-	
 	}
 	double speed=0.0;
 	double eff=0.0;
-	speed = speedup(gettime, time_avg);  //computes the speedup
-	eff = efficiency(speed, n_threads);  //computes the efficiency
+ //printf("time: %.9f ", gettime);
+	//speed = speedup(gettime, time_avg);  //computes the speedup
+	//eff = efficiency(speed, size);  //computes the efficiency
 	//printf("\nGettime:%.9f\nAvgTime:%.9f\nSpeedup:%.9f\nEfficiency:%.9f", gettime, time_avg, speed, eff);
+
 	if(gettime==0.0){ //handles the case where the sequential program is not run before this program
-     fprintf(avg, "BlockOmp%d,%d,%d,%.9f,-,-\n", blocksize, n, n_threads, time_avg);
+     fprintf(avg, "MPI,%d,%d,%.9f,-,-\n", n, size, time_avg);
    }
    else{
-	  fprintf(avg, "BlockOmp,%d,%d,%.9f,%.9f,%.9f\n", n, n_threads, time_avg, speed, eff);
+     speed = speedup(gettime, time_avg); 
+     eff = efficiency(speed, size);
+	  fprintf(avg, "MPI,%d,%d,%.9f,%.9f,%.9f\n", n, size, time_avg, speed, eff);
 	}
-	fclose(avg);
-	free(sort); 
- */
- 
+
+   fclose(avg);
+   free(sort); 
+   
+ }
+   //printf("end");
+//	  free(sort); 
+    //fclose(avg);
+    MPI_Finalize();
     return 0;
 }
